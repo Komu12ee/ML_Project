a7370b78d443c735b91cff2792e01a67545f83bc
komu <sahukomendra721@gmail.com>
2025-08-31 17:15:58 +0530

    wapis se start kr rh env setup issue

--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
 from setuptools import find_packages,setup
+from typing import List
 
-HYPHEN_E_DOT='-e .'
-def get_requirements(file_path:str)->list[str]:
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
-    
     '''
     requirements=[]
-    with open(file_path) as obj_file:
-        requirements=obj_file.readlines()
-        requirements=[req.replace("\n"," ") for req in requirements]
-        if HYPHEN_E_DOT in requirements:
-            requirements.remove(HYPHEN_E_DOT)
-    return requirements         
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
 
 setup(
-    name='mlproject',
-    version='0.0.1',
-    author='komendra',
-    author_email='sahukomendra721@gmail.com',
-    packages=find_packages(),
-    # install_requires=["pandas","numpy","seaborn","matplotlib"]    # not fesible to write all requirement like this so we have a different way for it
-    install_requires=get_requirements("requirement.txt")
-    
+name='mlproject',
+version='0.0.1',
+author='komendra',
+author_email='sahukomendra721@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
 )
+
