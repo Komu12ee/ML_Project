76dc5ce1b88c87ccd611c11cbda3e3dd66597317
komu <sahukomendra721@gmail.com>
2025-08-31 19:06:14 +0530

    yaha tk logger and exception file ka output log me save ho rha hai but condition laga ke check krna pdega aapko

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,4 @@
-import sys 
+import sys
 from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
@@ -17,4 +17,6 @@ class CustomException(Exception):
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
     
     def __str__(self):
-        return self.error_message 
+        return self.error_message
+    
+    
